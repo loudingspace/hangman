22de4a36f02c6d48859b3c447bb384dfb3d3b792
loudingspace <122986737+loudingspace@users.noreply.github.com>
2023-10-01 18:32:12 +0100

    Refactored and optimised current code for Task 7 of M2

--- milestone_2.py
+++ milestone_2.py
@@ -1,16 +1,37 @@
 import random
 import string
 
+# set up variables for game
 word_list = ['grapes', 'orange', 'plum', 'kiwi', 'banana']
-print(word_list)
+# print(word_list)
 word = random.choice(word_list)
-print(f'Random word is {word}')
-
-guess = input('Please guess a single letter: ')
+# print(f'Random word is {word}')
 alphabet = string.ascii_lowercase
 
 
-if len(guess) == 1 and guess.lower() in alphabet:
-    print('Good guess!')
-else:
-    print('Oops! That is not a valid input.')
+def get_input():
+    guess = input('Please guess a single letter: ')
+    return guess
+
+
+def validate_input(guess):
+    '''
+    Args:
+        guess: string, which should be one character and a alphabetical letter
+    Return:
+        True or False depending on whether it adheres to the conditions
+    '''
+    if len(guess) == 1 and guess.lower() in alphabet:
+        print('Good guess!')
+        return True
+    else:
+        print('Oops! That is not a valid input.')
+        return False
+
+
+def hangman():
+    guess = get_input()
+    validate_input(guess)
+
+
+hangman()
