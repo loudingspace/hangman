d25b9525ecd36ac98b6be8ed2ef4c025f45552db
loudingspace <122986737+loudingspace@users.noreply.github.com>
2023-10-01 18:24:18 +0100

    Completes tasks up to Task 6 of Milestone 2 and modifies the README.md file

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,16 @@
 import random
+import string
 
 word_list = ['grapes', 'orange', 'plum', 'kiwi', 'banana']
 print(word_list)
 word = random.choice(word_list)
 print(f'Random word is {word}')
+
+guess = input('Please guess a single letter: ')
+alphabet = string.ascii_lowercase
+
+
+if len(guess) == 1 and guess.lower() in alphabet:
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
