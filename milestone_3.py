37895af22b97e36c097e32a8a4ba42d573468c54
loudingspace <122986737+loudingspace@users.noreply.github.com>
2023-10-02 07:52:56 +0100

    Importing from milestone_2 was making the While loop not work as expected. Not sure why. So have just replicated code

--- milestone_3.py
+++ milestone_3.py
@@ -1,4 +1,19 @@
-import milestone_2
+import random
+
+word_list = ['grapes', 'orange', 'plum', 'kiwi', 'banana']
+
+
+def generate_random_word(word_list):
+    '''
+    args: a list of words
+
+    return: a word from that list
+    '''
+    return random.choice(word_list)
+
+
+word = generate_random_word(word_list)
+print(word)
 
 while True:
     guess = input('Please guess a single letter: ')
@@ -6,3 +21,9 @@ while True:
         break
     else:
         print('Invalid letter. Please, enter a single alphabetical character.')
+
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
