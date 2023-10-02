c656005db1dcd2e5ac16179c65ca668454532a72
loudingspace <122986737+loudingspace@users.noreply.github.com>
2023-10-02 08:13:19 +0100

    Updated documentation, so far as that is possible at this stage, and completed the Tasks up to this point

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Hangman is a classic game in which a player thinks of a word and the other playe
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it.
 
-TBC.
+The project currently generates a random word from a word list with the generate_random_word() function. It then asks for user input using the ask_for_input() function inviting the user to enter a letter. If this is alphanumeric and only one character it then checks this using the check_guess(guess) function which checks to see if this letter is contained in the randomly generated word..
+
+To run use python:
+
+    python milestone_3.py
--- milestone_3.py
+++ milestone_3.py
@@ -12,18 +12,35 @@ def generate_random_word(word_list):
     return random.choice(word_list)
 
 
+def check_guess(guess):
+    '''
+    Checks the guess inputted by user
+
+    arg guess:    a letter
+    return: boolean whether letter in word
+    '''
+
+    guess = guess.lower()
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+        return True
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        return False
+
+
+def ask_for_input():
+    while True:
+        guess = input('Please guess a single letter: ')
+        if len(guess) == 1 and guess.isalpha():  # the check is to see if it is valid,
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+
+    check_guess(guess)
+
+
 word = generate_random_word(word_list)
 print(word)
-
-while True:
-    guess = input('Please guess a single letter: ')
-    if len(guess) == 1 and guess.isalpha():
-        break
-    else:
-        print('Invalid letter. Please, enter a single alphabetical character.')
-
-
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+ask_for_input()
